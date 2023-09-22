f5f4e0dce1ad8850516fb210d1863f7b1f6939eb
Raul César <118937046+RaulCsar@users.noreply.github.com>
2023-09-22 10:38:07 -0300

    Primeiro commit

---
+++ App.js
@@ -0,0 +1,45 @@
+import * as React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import HomeScreen from "./screens/Home";
+import LoginScreen from "./screens/Login";
+import CadastroScreen from "./screens/Register";
+import NumberRegistScreen from "./screens/NumberRegist";
+import NumberEditScreen from "./screens/NumberEdit";
+const Stack = createNativeStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Login">
+        <Stack.Screen
+          name="Login"
+          component={LoginScreen}
+          options={{ title: "Login de Usuário" }}
+        />
+        <Stack.Screen
+          name="Register"
+          component={CadastroScreen}
+          options={{ title: "Cadastre-se" }}
+        />
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="NumRegist"
+          component={NumberRegistScreen}
+          options={{ headerShown: true, title: "Adicionar Contato" }}
+        />
+        <Stack.Screen
+          name="NumEdit"
+          component={NumberEditScreen}
+          options={{ headerShown: true, title: "Contato" }}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
